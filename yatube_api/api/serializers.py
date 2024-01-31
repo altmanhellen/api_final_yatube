5736fb8b13eae57d9dbcf89dd423a8b93c5f28d8
Elena Altman <altmanhellen@gmail.com>
2024-01-31 21:54:16 +0200

    Fix the code according to PEP8.

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -55,7 +55,7 @@ class FollowSerializer(serializers.ModelSerializer):
         queryset=Follow.objects.all(),
         fields=('user', 'following',),
         message='Вы уже подписаны на этого автора'
-    ),]
+    ), ]
 
     def validate(self, data):
         if self.context['request'].user != data.get('following'):
